d6d713b5e60c22a8dadc5347f9da1aaa886578b4
nurkhaliqfm <nurkhaliqfuthra@gmail.com>
2025-07-27 08:54:45 +0800

    remove unnecessary

--- src/hooks/useTypedSelector.ts
+++
@@ -1,4 +0,0 @@
-import type { RootState } from "@/redux/rootReducer";
-import { useSelector, type TypedUseSelectorHook } from "react-redux";
-
-export const useTypedSelector: TypedUseSelectorHook<RootState> = useSelector;
--- src/redux/rootReducer.ts
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux';
-import oauthReducer from '@/modules/auth/oauthSlice';
-
-const RootReducer = combineReducers({
-	oauth: oauthReducer,
-});
-
-export type RootState = ReturnType<typeof RootReducer>;
-export default RootReducer;
