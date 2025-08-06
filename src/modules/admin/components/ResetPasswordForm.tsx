31b9d4a956a95a778fa99df0d009e14bd559b32a
nurkhaliqfm <nurkhaliqfuthra@gmail.com>
2025-08-06 15:15:23 +0800

    ref: validation updated passpors=w=d

--- src/modules/admin/components/ResetPasswordForm.tsx
+++ src/modules/admin/components/ResetPasswordForm.tsx
@@ -9,10 +9,17 @@ import AppRoutes from "@/router/routes";
 import { PiEyeSlash, PiEye, PiLockKeyFill } from "react-icons/pi";
 import { updatePassword } from "../service/userService";
 
-const formOAuthScheme = z.object({
-	oldpassword: z.string().min(1, { message: "Password Lama is required" }),
-	newpassword: z.string().min(1, { message: "New Password is required" }),
-});
+const formOAuthScheme = z
+	.object({
+		oldpassword: z.string().min(8, "Password must be at least 8 characters"),
+		newpassword: z
+			.string()
+			.min(8, "New password must be at least 8 characters"),
+	})
+	.refine((data) => data.oldpassword !== data.newpassword, {
+		path: ["newpassword"],
+		message: "New password must be different from current password",
+	});
 
 export function ResetPasswordForm() {
 	const navigate = useNavigate();
@@ -68,7 +75,7 @@ export function ResetPasswordForm() {
 	}
 
 	return (
-		<Card className="py-4">
+		<Card className="py-4 shadow-none">
 			<CardBody className="grid p-0 md:grid-cols-1">
 				<div className="flex flex-col">
 					<div className="pt-2 pb-6 px-4 flex-col text-center">
@@ -138,7 +145,37 @@ export function ResetPasswordForm() {
 							isInvalid={!!errors["newpassword"]?.message}
 						/>
 
-						<div className="flex justify-end w-full">
+						<div className="flex justify-end gap-2 w-lg">
+							<Button
+								isLoading={isLoading}
+								spinner={
+									<svg
+										className="animate-spin h-5 w-5 text-current"
+										fill="none"
+										viewBox="0 0 24 24"
+										xmlns="http://www.w3.org/2000/svg">
+										<circle
+											className="opacity-25"
+											cx="12"
+											cy="12"
+											r="10"
+											stroke="currentColor"
+											strokeWidth="4"
+										/>
+										<path
+											className="opacity-75"
+											d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+											fill="currentColor"
+										/>
+									</svg>
+								}
+								className="mt-4 w-full text-danger"
+								color="danger"
+								variant="ghost"
+								size="lg"
+								onPress={() => navigate(AppRoutes.AdminDashboard.path)}>
+								Batal
+							</Button>
 							<Button
 								isLoading={isLoading}
 								spinner={
--- src/modules/admin/pages/ResetPasswordPage.tsx
+++ src/modules/admin/pages/ResetPasswordPage.tsx
@@ -2,8 +2,8 @@ import { ResetPasswordForm } from "../components/ResetPasswordForm";
 
 function ResetPasswordPage() {
 	return (
-		<div className="flex min-h-svh flex-col items-center justify-center bg-muted p-6 md:p-10">
-			<div className="w-full max-w-sm md:max-w-3xl">
+		<div className="flex flex-col mt-10 md:mt-14 bg-public-secondary items-center justify-center bg-muted p-6 md:p-10">
+			<div className="w-full max-w-sm md:max-w-3xl ">
 				<ResetPasswordForm />
 			</div>
 		</div>
