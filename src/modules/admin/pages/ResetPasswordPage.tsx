14a6832901d40fbf2bf1f9193e96e6856a005371
nurkhaliqfm <nurkhaliqfuthra@gmail.com>
2025-08-06 15:28:36 +0800

    ref: style update password page

--- src/modules/admin/pages/ResetPasswordPage.tsx
+++ src/modules/admin/pages/ResetPasswordPage.tsx
@@ -2,7 +2,7 @@ import { ResetPasswordForm } from "../components/ResetPasswordForm";
 
 function ResetPasswordPage() {
 	return (
-		<div className="flex flex-col mt-10 md:mt-14 bg-public-secondary items-center justify-center bg-muted p-6 md:p-10">
+		<div className="flex flex-col mt-10 md:mt-14 rounded-2xl bg-public-secondary items-center justify-center bg-muted p-6 md:p-10">
 			<div className="w-full max-w-sm md:max-w-3xl ">
 				<ResetPasswordForm />
 			</div>
